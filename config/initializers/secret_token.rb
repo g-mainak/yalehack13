59c600c192baa9d47fbee4addf544fa3dba9196d
Mainak Ghosh <g.mainak@gmail.com>
2013-02-23 16:08:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yalehack13::Application.config.secret_token = 'c6c2397ba47b770b64c1a89f60da56d3d3208c4dda9c20b67cf44da6a4ee0a655e9ae95331658c16bd792e8f1d6f3c6a4d23371e75d070736e16222384465888'
