be37b51a19049d440932cd2596ab2cad607e321e
Mainak Ghosh <g.mainak@gmail.com>
2013-02-24 01:12:13 -0500

    twitter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 
 	def create
 		auth_hash = request.env['omniauth.auth']
+		str = ""
 		auth_hash.each do |key, array|
   			str = str + key + ":" + array +";\n"
 		end
