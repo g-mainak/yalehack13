f8a8c218f4648d9f8f660e5f265588d608db0845
Mainak Ghosh <g.mainak@gmail.com>
2013-02-24 04:09:59 -0500

    Make users look pretty

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,29 +2,28 @@ class User < ActiveRecord::Base
 	has_many :comments
 	attr_accessible :name, :netid, :bio, :skills
 	
-  def self.create_with_omniauth(auth)
-	  create! do |user|
-	    user.provider = auth['provider']
-	    user.uid = auth['uid']
-	    if auth['info']
-	       user.name = auth['info']['name'] || ""
-	    end
-	  end
+	def self.create_with_omniauth(auth)
+		create! do |user|
+			user.provider = auth['provider']
+			user.uid = auth['uid']
+			if auth['info']
+				user.name = auth['info']['name'] || ""
+			end
+		end
 	end
 	has_many :authorizations
 
 
 	def add_provider(auth_hash)
-  # Check if the provider already exists, so we don't add it twice
-  unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-  	if auth_hash["provider"]=="twitter"
-      st = auth_hash["uid"] + ',' + auth_hash.credentials.secret + ',' + auth_hash.credentials.token
-      Authorization.create :user => self, :provider => auth_hash["provider"], :uid => st
-  	else
-  		Authorization.create :user => self, :provider => auth_hash["provider"], :uid => st
-  	end
-  end
-end
->>>>>>> mainak
+	  # Check if the provider already exists, so we don't add it twice
+	  unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+	  	if auth_hash["provider"]=="twitter"
+	  		st = auth_hash["uid"] + ',' + auth_hash.credentials.secret + ',' + auth_hash.credentials.token
+	  		Authorization.create :user => self, :provider => auth_hash["provider"], :uid => st
+	  	else
+	  		Authorization.create :user => self, :provider => auth_hash["provider"], :uid => st
+	  	end
+	  end
+	end
 end
 
