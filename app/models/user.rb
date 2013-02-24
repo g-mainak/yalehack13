9d838d167415754362aebffc6be3c0b07fac5457
Hugh O'Cinneide <hkocinneide@gmail.com>
2013-02-24 01:41:09 -0500

    Fixing some merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
 	has_many :comments
-
+	attr_accessible :name, :netid, :bio, :skills
+	
   def self.create_with_omniauth(auth)
 	  create! do |user|
 	    user.provider = auth['provider']
@@ -11,16 +11,14 @@ class User < ActiveRecord::Base
 	    end
 	  end
 	end
-=======
 	has_many :authorizations
-	attr_accessible :name, :netid, :bio, :skills
+
 
 	def add_provider(auth_hash)
-  # Check if the provider already exists, so we don't add it twice
-  unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-  	Authorization.create :user => self, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-  end
-end
->>>>>>> c06343a3ec123902505f36ffb2de1cd08e714aeb
+	  # Check if the provider already exists, so we don't add it twice
+	  unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+	  	Authorization.create :user => self, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
+	  end
+	end
 end
 
