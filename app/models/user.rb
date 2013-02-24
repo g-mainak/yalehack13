8f522f07c81f6868ae99ce195c732f6e5956d42a
Mainak Ghosh <g.mainak@gmail.com>
2013-02-24 04:49:44 -0500

    Added emails and netids

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,27 @@
 class User < ActiveRecord::Base
-require 'net/ldap'
+# require 'net/ldap'
 
 	has_many :comments
-	attr_accessible :name, :netid, :bio, :skills
+	attr_accessible :name, :netid, :bio, :skills, :email
 	
 
 	has_many :authorizations
-  def self.create_with_omniauth(auth)
-	  create! do |user|
-	    user.provider = auth['provider']
-	    user.uid = auth['uid']
-	    if auth['info']
-	       user.name = auth['info']['name'] || ""
-	    end
-	  end
-	end
+ #  def self.create_with_omniauth(auth)
+	#   create! do |user|
+	#     user.provider = auth['provider']
+	#     user.uid = auth['uid']
+	#     if auth['info']
+	#        user.name = auth['info']['name'] || ""
+	#     end
+	#   end
+	# end
 
-	after_create :populateLDAP
+	# after_create :populateLDAP
 
 	
-	def name
-		self.fname.capitalize + " " + self.lname.capitalize
-	end
+	# def name
+	# 	self.fname.capitalize + " " + self.lname.capitalize
+	# end
 
 	def add_provider(auth_hash)
 	  # Check if the provider already exists, so we don't add it twice
@@ -35,55 +35,55 @@ require 'net/ldap'
 	  end
 	end
 
-	protected
+	# protected
 
-	  def populateLDAP
+	#   def populateLDAP
     
-    #quit if no email or netid to work with
-    self.email ||= ''
-    return if !self.email.include?('@yale.edu') && !self.netid
+ #    #quit if no email or netid to work with
+ #    self.email ||= ''
+ #    return if !self.email.include?('@yale.edu') && !self.netid
  
-    begin
-      ldap = Net::LDAP.new( :host =>"directory.yale.edu" , :port =>"389" )
+ #    begin
+ #      ldap = Net::LDAP.new( :host =>"directory.yale.edu" , :port =>"389" )
  
-      #set e filter
-      if !self.email.blank?
-        f = Net::LDAP::Filter.eq('mail', self.email)
-      else #netid
-        f = Net::LDAP::Filter.eq('uid', self.netid)
-      end
+ #      #set e filter
+ #      if !self.email.blank?
+ #        f = Net::LDAP::Filter.eq('mail', self.email)
+ #      else #netid
+ #        f = Net::LDAP::Filter.eq('uid', self.netid)
+ #      end
  
-      b = 'ou=People,o=yale.edu'
-      p = ldap.search(:base => b, :filter => f, :return_result => true).first
+ #      b = 'ou=People,o=yale.edu'
+ #      p = ldap.search(:base => b, :filter => f, :return_result => true).first
     
-    rescue Exception => e
-          logger.debug :text => e
-          logger.debug :text => "*** ERROR with LDAP"
-          guessFromEmail
-    end
+ #    rescue Exception => e
+ #          logger.debug :text => e
+ #          logger.debug :text => "*** ERROR with LDAP"
+ #          guessFromEmail
+ #    end
   
-    self.netid = ( p['uid'] ? p['uid'][0] : '' )
-    self.fname = ( p['knownAs'] ? p['knownAs'][0] : '' )
-    if self.fname.blank?
-      self.fname = ( p['givenname'] ? p['givenname'][0] : '' )
-    end
-    self.lname = ( p['sn'] ? p['sn'][0] : '' )
-    self.email = ( p['mail'] ? p['mail'][0] : '' )
-    self.year = ( p['class'] ? p['class'][0].to_i : 0 )
-    self.college = ( p['college'] ? p['college'][0] : '' )
-    self.save!
+ #    self.netid = ( p['uid'] ? p['uid'][0] : '' )
+ #    self.fname = ( p['knownAs'] ? p['knownAs'][0] : '' )
+ #    if self.fname.blank?
+ #      self.fname = ( p['givenname'] ? p['givenname'][0] : '' )
+ #    end
+ #    self.lname = ( p['sn'] ? p['sn'][0] : '' )
+ #    self.email = ( p['mail'] ? p['mail'][0] : '' )
+ #    self.year = ( p['class'] ? p['class'][0].to_i : 0 )
+ #    self.college = ( p['college'] ? p['college'][0] : '' )
+ #    self.save!
  
-  end
+ #  end
 
-	  # not a yale email, just make best guess at it 
-  def guessFromEmail
-    name = self.email[ /[^@]+/ ]
-    return false if !name
-    name = name.split( "." )
+	#   # not a yale email, just make best guess at it 
+ #  def guessFromEmail
+ #    name = self.email[ /[^@]+/ ]
+ #    return false if !name
+ #    name = name.split( "." )
  
-    self.fname = name[0].downcase
-    self.lname = name[1].downcase || ''
-    self.save
-  end
+ #    self.fname = name[0].downcase
+ #    self.lname = name[1].downcase || ''
+ #    self.save
+ #  end
 end
 
