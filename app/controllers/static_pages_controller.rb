a8f1210afe3134773388d32e3201dbc964ef40c2
Mainak Ghosh <g.mainak@gmail.com>
2013-02-23 19:41:03 -0500

    CASified

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
 	
 	def home
+		@username = session[:cas_user]
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(:version => 20130223223739) do
     t.datetime "updated_at",   :null => false
   end
 
+  create_table "templates", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "netid"
