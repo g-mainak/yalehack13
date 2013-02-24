fcb70aaee19c23fd71b9ae6dd2182423211e231f
Mainak Ghosh <g.mainak@gmail.com>
2013-02-24 02:59:37 -0500

    twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :netid, :bio, :skills
 
 	def add_provider(auth_hash)
-  # Check if the provider already exists, so we don't add it twice
-  unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
     puts "===GGGGG======++++++++++++++++++=========="
     puts auth_hash["provider"]+auth_hash["credentials"]
+  # Check if the provider already exists, so we don't add it twice
+  unless authorizations.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
   	if auth_hash["provider"]=="twitter"
   		Authorization.create :user => self, :provider => auth_hash["provider"], :uid => auth_hash["uid"]+','+auth_hash["credentials"]["token"]+','+auth_hash["credentials"]["secret"]
   	else
