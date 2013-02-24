e4a8bbffda47edd3012291d6b14e4d2ecb56e536
Hugh O'Cinneide <hkocinneide@gmail.com>
2013-02-24 12:39:05 -0500

    fixing this bitch

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,7 @@
 class Project < ActiveRecord::Base
 	has_many :comments
 	
-<<<<<<< HEAD
+
 	attr_accessible :project_name, :founder_name, :looking_for, :description, :rating, :consider, :photo
 
-=======
-	attr_accessible :project_name, :founder_name, 
-	:looking_for, :description, :rating, :consider
->>>>>>> 01cce2d38eb53f258950e893a881d4c783b31db3
 end
