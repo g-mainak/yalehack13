58941298bc460fac29af8a0b64cb8708b79772b8
Brandon Smith <brandon.smith@yale.edu>
2013-03-24 01:51:31 -0400

    updated header with account dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,8 @@ class ApplicationController < ActionController::Base
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
   end
+
+  def signed_in?
+    !current_user.nil?
+  end
 end
